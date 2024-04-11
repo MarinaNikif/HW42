4d8bb6af5d78101f633d681dcdf6b200ada3ee7f
MarinaNikif <117200047+MarinaNikif@users.noreply.github.com>
2024-04-11 13:17:46 +0300

    Update HW43.py

--- HW43.py
+++ HW43.py
@@ -1,30 +1,40 @@
 import json
 
+def normalize_key(key):
+    return key.lower().replace('_', '')
+
+def normalize_data(employee):
+    return {normalize_key(k): v for k, v in employee.items()}
+
 def employees_rewrite(sort_type):
-    # Загрузка данных из файла employees.json
     with open('employees.json', 'r') as file:
         data = json.load(file)
 
-    # Проверка наличия указанного ключа для сортировки
-    if sort_type not in data['employees'][0]:
-        raise ValueError('Bad key for sorting')
+    normalized_data = []
+    for employee in data['employees']:
+        normalized_employee = {}
+        for k, v in employee.items():
+            normalized_employee[normalize_key(k)] = v
+        normalized_data.append(normalized_employee)
 
-    # Сортировка данных по указанному ключу
-    sorted_data = sorted(data['employees'], key=lambda x: x[sort_type], reverse=(isinstance(data['employees'][0][sort_type], int)))
+    lower_sort_type = normalize_key(sort_type)
+    if lower_sort_type not in normalized_data[0]:
+        raise ValueError(f'Key "{sort_type}" not found for sorting')
 
-    # Запись отсортированных данных в новый файл
+    sorted_data = sorted(normalized_data, key=lambda x: x[lower_sort_type])
+
     output_filename = f'employees_{sort_type}_sorted.json'
     with open(output_filename, 'w') as output_file:
         json.dump({"employees": sorted_data}, output_file, indent=4)
 
-# Вызов функции для сортировки по фамилии (lastName)
-employees_rewrite('lastName')
+# Вызов функции для сортировки по фамилии ('lastName')
+employees_rewrite('lastname')
 
 # Вызов функции для сортировки по имени (firstName)
-employees_rewrite('firstName')
+employees_rewrite('FIRSTName')
 
 # Вызов функции для сортировки по отделу (department)
 employees_rewrite('department')
 
 # Вызов функции для сортировки по зарплате (salary)
-employees_rewrite('salary')
+employees_rewrite('SaLaRy')
